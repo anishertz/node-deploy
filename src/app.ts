f1b1b2674dafc7ab27be9a830839ce1c0240e77b
Anish Chaulagain <anish.chaulagain@monotype.com>
2026-02-12 12:18:43 +0545

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/health', (_, res) => res.sendStatus(200));
 
 app.get('/', (req, res) => {
-  res.send('Nodejs deployment to AWS using jenkins');
+  res.send('Nodejs deployment to AWS using jenkins updated recently');
 });
 
 export default app;
