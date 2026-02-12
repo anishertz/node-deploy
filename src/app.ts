3f9d97f8aaff82001fa9ef8fa34c2e070dc0fc7e
Anish Chaulagain <anish.chaulagain@monotype.com>
2026-02-12 14:43:15 +0545

    create pipeline with jenkins

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/health', (_, res) => res.sendStatus(200));
 
 app.get('/', (req, res) => {
-  res.send('Nodejs deployment to AWS using jenkins recently updated');
+  res.send('Nodejs deployment to AWS');
 });
 
 export default app;
